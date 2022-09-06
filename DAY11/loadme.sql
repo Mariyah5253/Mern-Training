6cf5e3233e31c391ab6c5c0e0be8eaebe6b7ec2c
mariyah5253 <mariya.johar.hussain@gmail.com>
2022-09-06 17:01:19 +0530

    Module 2 work updates

---
+++ DAY11/loadme.sql
@@ -0,0 +1,4 @@
+ insert into my_employee values(3,'BIRI','BEN', concat(substr(first_name,1,1),substr(last_name,1,7)) ,1100);
+ insert into my_employee values(4,'NEWMAN','CHAD', concat(substr(first_name,1,1),substr(last_name,1,7)) ,750);
+ insert into my_employee values(5,'ROPEBURN','AUDRY', concat(substr(first_name,1,1),substr(last_name,1,7)) ,1550);
+ insert into my_employee values(6,'Hussain','mariya',concat(substr(first_name,1,1),substr(last_name,1,7)),4100);
